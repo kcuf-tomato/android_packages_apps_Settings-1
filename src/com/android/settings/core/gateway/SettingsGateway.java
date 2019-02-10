nitrogen-project/android_packages_apps_Settings

add activity to launch headsup settings fragment

src/com/android/settings/core/gateway/SettingsGateway.java
@@ -137,6 +137,7 @@ import com.android.settings.wifi.WifiSettings;
 import com.android.settings.wifi.calling.WifiCallingSettings;
 import com.android.settings.wifi.p2p.WifiP2pSettings;
 
+import com.nitrogen.settings.fragments.HeadsUpSettings;
 import com.nitrogen.settings.NitrogenSettings;
 
 public class SettingsGateway {
@@ -263,7 +264,8 @@ public class SettingsGateway {
             DirectoryAccessDetails.class.getName(),
             ToggleBackupSettingFragment.class.getName(),
             PreviouslyConnectedDeviceDashboardFragment.class.getName(),
-            NitrogenSettings.class.getName()
+            NitrogenSettings.class.getName(),
+            HeadsUpSettings.class.getName()
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {

kcuf-tomato/android_packages_apps_Settings-1

src/com/android/settings/core/gateway/SettingsGateway.java
@@ -139,6 +139,7 @@ import com.android.settings.wifi.p2p.WifiP2pSettings;
 import com.android.settings.wifi.tether.WifiTetherSettings;
 
 import com.stag.horns.Horns;
+import com.stag.horns.fragments.HeadsUpSettings;
 
 public class SettingsGateway {
 
@@ -266,6 +267,7 @@ public class SettingsGateway {
             ToggleBackupSettingFragment.class.getName(),
             PreviouslyConnectedDeviceDashboardFragment.class.getName(),
             Horns.class.getName(),
+            HeadsUpSettings.class.getName()
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {