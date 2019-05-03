ca2a6596e25527b311e136b23c82c4d885e10f45
Henrique Silva <jhenrique09.mcz@hotmail.com>
2019-05-03 14:18:10 -0300

    Settings: Changes for LiveDisplay
    All credits to LineageOS and related authors on:
    
    https://github.com/LineageOS/android_packages_apps_LineageParts
    
    Change-Id: I177e825d130808d7cedef85e09147f0ed70326ae
    Signed-off-by: Henrique Silva <jhenrique09.mcz@hotmail.com>
    
    spezi77: Add CustomDialogPreference.

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -172,4 +172,8 @@ public class Settings extends SettingsActivity {
     public static class SmartbarSettingsActivity extends SettingsActivity {}
     public static class FlingSettingsActivity extends SettingsActivity {}
     public static class PulseSettingsActivity extends SettingsActivity {}
+
+    // LiveDisplay
+    public static class LiveDisplaySettingsActivity extends SettingsActivity { /* empty */ }
+
 }
