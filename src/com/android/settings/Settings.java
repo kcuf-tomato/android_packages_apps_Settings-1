42e2fe5f7474680c1286328fed2d762e8fa84aca
Adin Kwok <adin.kwok@carbonrom.org>
2019-05-16 21:00:18 -0400

    Settings: External Smart Pixels fragment launch
    Necessary for Smart Pixels tile.
    Based on change by Mazda--.
    
    Reference commit:
    DirtyUnicorns@c637500b89bfe1b726a92efa358cf48b1a2a3093
    
    Change-Id: I5a1892d8211ac968958395326fbbbfcdfcf7e942

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -172,4 +172,5 @@ public class Settings extends SettingsActivity {
     public static class SmartbarSettingsActivity extends SettingsActivity {}
     public static class FlingSettingsActivity extends SettingsActivity {}
     public static class PulseSettingsActivity extends SettingsActivity {}
+    public static class SmartPixelsActivity extends SettingsActivity {}
 }
