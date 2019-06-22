calebcabob/packages_apps_Settings

Settings: External Smart Pixels fragment launch
Necessary for Smart Pixels tile.
Based on change by Mazda--.

Reference commit:
DirtyUnicorns@c637500b89bfe1b726a92efa358cf48b1a2a3093

Change-Id: I5a1892d8211ac968958395326fbbbfcdfcf7e942

src/com/android/settings/core/gateway/SettingsGateway.java
@@ -143,6 +143,7 @@ import com.android.settings.wifi.p2p.WifiP2pSettings;
 
 import com.aquarios.coralreef.CoralReef;
 import com.aquarios.coralreef.fragments.CustomHeaders;
+import com.aquarios.coralreef.fragments.SmartPixels;
 import com.android.settings.wifi.tether.WifiTetherSettings;
 
 public class SettingsGateway {
@@ -276,7 +277,8 @@ public class SettingsGateway {
             FlingSettings.class.getName(),
             PulseSettings.class.getName(),
             SwipeUpGestureSettings.class.getName(),
-            CustomHeaders.class.getName()
+            CustomHeaders.class.getName(),
+            SmartPixels.class.getName()
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {
@@ -322,6 +324,7 @@ public class SettingsGateway {
             Settings.PulseSettingsActivity.class.getName(),
             Settings.StockNavigationActivity.class.getName(),
             Settings.CustomHeadersActivity.class.getName(),
-            Settings.SwipeToNotificationSettingsActivity.class.getName()
+            Settings.SwipeToNotificationSettingsActivity.class.getName(),
+            Settings.SmartPixelsActivity.class.getName()
     };
 }

kcuf-tomato/android_packages_apps_Settings-1

src/com/android/settings/core/gateway/SettingsGateway.java
@@ -144,6 +144,7 @@ import com.android.settings.wifi.tether.WifiTetherSettings;
 
 import com.stag.horns.Horns;
 import com.stag.horns.fragments.HeadsUpSettings;
+import com.stag.horns.fragments.SmartPixels;
 
 public class SettingsGateway {
 
@@ -275,7 +276,8 @@ public class SettingsGateway {
             NavigationSettingsActivity.class.getName(),
             SmartbarSettingsActivity.class.getName(),
             FlingSettingsActivity.class.getName(),
-            PulseSettingsActivity.class.getName()
+            PulseSettingsActivity.class.getName(),
+            SmartPixels.class.getName()
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {
@@ -319,6 +321,7 @@ public class SettingsGateway {
             Settings.NavigationSettingsActivity.class.getName(),
             Settings.SmartbarSettingsActivity.class.getName(),
             Settings.FlingSettingsActivity.class.getName(),
-            Settings.PulseSettingsActivity.class.getName()
+            Settings.PulseSettingsActivity.class.getName(),
+            Settings.SmartPixelsActivity.class.getName()
     };
 }