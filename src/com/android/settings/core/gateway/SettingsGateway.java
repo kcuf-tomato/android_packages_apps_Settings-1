73dca48d65f4497036c0b1c1ad6258b16583f1bc
SKULSHADY <anushekprasal@gmail.com>
2019-07-02 14:33:56 +0530

    Settings: Double tap to trigger doze [2/2]
    * Also adapt Add video to Double tap to trigger doze from Pie: https://github.com/Evolution-X/packages_apps_Settings/commit/a66601a6d41e7cd49efe68d294f9c10491621c16
    
    Change-Id: Id98543542db937c6cc1f176cd55f6b02e472849d

--- src/com/android/settings/core/gateway/SettingsGateway.java
+++ src/com/android/settings/core/gateway/SettingsGateway.java
@@ -287,7 +287,7 @@ public class SettingsGateway {
             MobileNetworkListFragment.class.getName(),
             GlobalActionsPanelSettings.class.getName(),
             DarkModeSettingsFragment.class.getName(),
-            Horns.class.getName()
+            Horns.class.getName(),
             GamingMode.class.getName(),
     };
 
