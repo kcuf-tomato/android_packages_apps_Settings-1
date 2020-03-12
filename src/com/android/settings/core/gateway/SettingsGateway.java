whyred-11/packages_apps_Settings

Add SmartPixels Activity

src/com/android/settings/core/gateway/SettingsGateway.java
@@ -149,6 +149,7 @@ import com.android.settings.wifi.p2p.WifiP2pSettings;
 import com.android.settings.wifi.savedaccesspoints.SavedAccessPointsWifiSettings;
 import com.android.settings.wifi.tether.WifiTetherSettings;
 import com.ion.ionizer.fragments.HeadsUpSettings;
+import com.ion.ionizer.fragments.SmartPixels;
 import com.ion.ionizer.fragments.System;
 import com.ion.ionizer.IonIonizer;
 
@@ -292,6 +293,7 @@ public class SettingsGateway {
             HeadsUpSettings.class.getName(),
             System.class.getName(),
             DisplayRotation.class.getName(),
+            SmartPixels.class.getName(),
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {

kcuf-tomato/android_packages_apps_Settings-1

Change-Id: I270299d7bd1ed11188aaae8d3da137dd03ffd208

src/com/android/settings/core/gateway/SettingsGateway.java
@@ -148,10 +148,9 @@ import com.android.settings.wifi.calling.WifiCallingSettings;
 import com.android.settings.wifi.p2p.WifiP2pSettings;
 import com.android.settings.wifi.savedaccesspoints.SavedAccessPointsWifiSettings;
 import com.android.settings.wifi.tether.WifiTetherSettings;
-
 import com.stag.horns.Horns;
 import com.stag.horns.fragments.GamingMode;
-
+import com.stag.horns.fragments.SmartPixels;
 public class SettingsGateway {
 
     /**
@@ -291,6 +290,7 @@ public class SettingsGateway {
             Horns.class.getName(),
             GamingMode.class.getName(),
             DisplayRotation.class.getName(),
+            SmartPixels.class.getName(),
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {