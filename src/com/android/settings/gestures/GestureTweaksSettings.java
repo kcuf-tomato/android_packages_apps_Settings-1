ezio84/abc-n_packages_apps_Settings

Allow long back swipe to trigger custom actions/app/activities [2/2]

src/com/android/settings/gestures/GestureTweaksSettings.java
@@ -1,0 +1,188 @@
+/*
+ * Copyright (C) 2020 abcduwhatever
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.gestures;
+
+import android.content.ContentResolver;
+import android.content.Context;
+import android.os.Bundle;
+import android.os.UserHandle;
+import android.provider.SearchIndexableResource;
+import android.provider.Settings;
+import androidx.preference.PreferenceCategory;
+import androidx.preference.ListPreference;
+import androidx.preference.Preference;
+import androidx.preference.PreferenceScreen;
+import androidx.preference.Preference.OnPreferenceChangeListener;
+import androidx.preference.SwitchPreference;
+
+import com.android.internal.logging.nano.MetricsProto;
+
+import com.android.settings.R;
+import com.android.settings.search.BaseSearchIndexProvider;
+import com.android.settings.search.Indexable;
+import com.android.settings.SettingsPreferenceFragment;
+import com.android.settings.Utils;
+
+import com.abc.support.preferences.CustomSeekBarPreference;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GestureTweaksSettings extends SettingsPreferenceFragment
+        implements Preference.OnPreferenceChangeListener, Indexable {
+
+    private int leftSwipeActions;
+    private int rightSwipeActions;
+
+    private ListPreference mLeftSwipeActions;
+    private ListPreference mRightSwipeActions;
+    private Preference mLeftSwipeAppSelection;
+    private Preference mRightSwipeAppSelection;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.gesture_nav_tweaks);
+
+        final ContentResolver resolver = getActivity().getContentResolver();
+
+        leftSwipeActions = Settings.System.getIntForUser(resolver,
+                Settings.System.LEFT_LONG_BACK_SWIPE_ACTION, 0,
+                UserHandle.USER_CURRENT);
+        mLeftSwipeActions = (ListPreference) findPreference("left_swipe_actions");
+        mLeftSwipeActions.setValue(Integer.toString(leftSwipeActions));
+        mLeftSwipeActions.setSummary(mLeftSwipeActions.getEntry());
+        mLeftSwipeActions.setOnPreferenceChangeListener(this);
+
+        rightSwipeActions = Settings.System.getIntForUser(resolver,
+                Settings.System.RIGHT_LONG_BACK_SWIPE_ACTION, 0,
+                UserHandle.USER_CURRENT);
+        mRightSwipeActions = (ListPreference) findPreference("right_swipe_actions");
+        mRightSwipeActions.setValue(Integer.toString(rightSwipeActions));
+        mRightSwipeActions.setSummary(mRightSwipeActions.getEntry());
+        mRightSwipeActions.setOnPreferenceChangeListener(this);
+
+        mLeftSwipeAppSelection = (Preference) findPreference("left_swipe_app_action");
+        boolean isAppSelection = Settings.System.getIntForUser(resolver,
+                Settings.System.LEFT_LONG_BACK_SWIPE_ACTION, 0, UserHandle.USER_CURRENT) == 5/*action_app_action*/;
+        mLeftSwipeAppSelection.setEnabled(isAppSelection);
+
+        mRightSwipeAppSelection = (Preference) findPreference("right_swipe_app_action");
+        isAppSelection = Settings.System.getIntForUser(resolver,
+                Settings.System.RIGHT_LONG_BACK_SWIPE_ACTION, 0, UserHandle.USER_CURRENT) == 5/*action_app_action*/;
+        mRightSwipeAppSelection.setEnabled(isAppSelection);
+        customAppCheck();
+    }
+
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (preference == mLeftSwipeActions) {
+            int leftSwipeActions = Integer.valueOf((String) newValue);
+            Settings.System.putIntForUser(getContentResolver(),
+                    Settings.System.LEFT_LONG_BACK_SWIPE_ACTION, leftSwipeActions,
+                    UserHandle.USER_CURRENT);
+            int index = mLeftSwipeActions.findIndexOfValue((String) newValue);
+            mLeftSwipeActions.setSummary(
+                    mLeftSwipeActions.getEntries()[index]);
+            mLeftSwipeAppSelection.setEnabled(leftSwipeActions == 5);
+            customAppCheck();
+            return true;
+        } else if (preference == mRightSwipeActions) {
+            int rightSwipeActions = Integer.valueOf((String) newValue);
+            Settings.System.putIntForUser(getContentResolver(),
+                    Settings.System.RIGHT_LONG_BACK_SWIPE_ACTION, rightSwipeActions,
+                    UserHandle.USER_CURRENT);
+            int index = mRightSwipeActions.findIndexOfValue((String) newValue);
+            mRightSwipeActions.setSummary(
+                    mRightSwipeActions.getEntries()[index]);
+            mRightSwipeAppSelection.setEnabled(rightSwipeActions == 5);
+            customAppCheck();
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        // Ensure preferences sensible to change get updated
+        actionPreferenceReload();
+        customAppCheck();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        // Ensure preferences sensible to change gets updated
+        actionPreferenceReload();
+        customAppCheck();
+    }
+
+    /* Helper for reloading both short and long gesture as they might change on
+       package uninstallation */
+    private void actionPreferenceReload() {
+        int leftSwipeActions = Settings.System.getIntForUser(getContentResolver(),
+                Settings.System.LEFT_LONG_BACK_SWIPE_ACTION, 0,
+                UserHandle.USER_CURRENT);
+
+        int rightSwipeActions = Settings.System.getIntForUser(getContentResolver(),
+                Settings.System.RIGHT_LONG_BACK_SWIPE_ACTION, 0,
+                UserHandle.USER_CURRENT);
+
+        // Reload the action preferences
+        mLeftSwipeActions.setValue(Integer.toString(leftSwipeActions));
+        mLeftSwipeActions.setSummary(mLeftSwipeActions.getEntry());
+
+        mRightSwipeActions.setValue(Integer.toString(rightSwipeActions));
+        mRightSwipeActions.setSummary(mRightSwipeActions.getEntry());
+
+        mLeftSwipeAppSelection.setEnabled(mLeftSwipeActions.getEntryValues()
+                [leftSwipeActions].equals("5"));
+        mRightSwipeAppSelection.setEnabled(mRightSwipeActions.getEntryValues()
+                [rightSwipeActions].equals("5"));
+    }
+
+    private void customAppCheck() {
+        mLeftSwipeAppSelection.setSummary(Settings.System.getStringForUser(getContentResolver(),
+                String.valueOf(Settings.System.LEFT_LONG_BACK_SWIPE_APP_FR_ACTION), UserHandle.USER_CURRENT));
+        mRightSwipeAppSelection.setSummary(Settings.System.getStringForUser(getContentResolver(),
+                String.valueOf(Settings.System.RIGHT_LONG_BACK_SWIPE_APP_FR_ACTION), UserHandle.USER_CURRENT));
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsProto.MetricsEvent.ABC;
+    }
+
+    public static final SearchIndexProvider SEARCH_INDEX_DATA_PROVIDER =
+            new BaseSearchIndexProvider() {
+                @Override
+                public List<SearchIndexableResource> getXmlResourcesToIndex(Context context,
+                        boolean enabled) {
+                    final ArrayList<SearchIndexableResource> result = new ArrayList<>();
+                    final SearchIndexableResource sir = new SearchIndexableResource(context);
+                    sir.xmlResId = R.xml.gesture_nav_tweaks;
+                    result.add(sir);
+                    return result;
+                }
+
+                @Override
+                public List<String> getNonIndexableKeys(Context context) {
+                    final List<String> keys = super.getNonIndexableKeys(context);
+                    return keys;
+                }
+            };
+}

kcuf-tomato/android_packages_apps_Settings-1

Signed-off-by: spezi77 <spezi7713@gmx.net>

src/com/android/settings/gestures/GestureTweaksSettings.java
@@ -1,0 +1,188 @@
+/*
+ * Copyright (C) 2020 abcduwhatever
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.gestures;
+
+import android.content.ContentResolver;
+import android.content.Context;
+import android.os.Bundle;
+import android.os.UserHandle;
+import android.provider.SearchIndexableResource;
+import android.provider.Settings;
+import androidx.preference.PreferenceCategory;
+import androidx.preference.ListPreference;
+import androidx.preference.Preference;
+import androidx.preference.PreferenceScreen;
+import androidx.preference.Preference.OnPreferenceChangeListener;
+import androidx.preference.SwitchPreference;
+
+import com.android.internal.logging.nano.MetricsProto;
+
+import com.android.settings.R;
+import com.android.settings.search.BaseSearchIndexProvider;
+import com.android.settings.search.Indexable;
+import com.android.settings.SettingsPreferenceFragment;
+import com.android.settings.Utils;
+
+import com.stag.horns.preferences.CustomSeekBarPreference;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GestureTweaksSettings extends SettingsPreferenceFragment
+        implements Preference.OnPreferenceChangeListener, Indexable {
+
+    private int leftSwipeActions;
+    private int rightSwipeActions;
+
+    private ListPreference mLeftSwipeActions;
+    private ListPreference mRightSwipeActions;
+    private Preference mLeftSwipeAppSelection;
+    private Preference mRightSwipeAppSelection;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.gesture_nav_tweaks);
+
+        final ContentResolver resolver = getActivity().getContentResolver();
+
+        leftSwipeActions = Settings.System.getIntForUser(resolver,
+                Settings.System.LEFT_LONG_BACK_SWIPE_ACTION, 0,
+                UserHandle.USER_CURRENT);
+        mLeftSwipeActions = (ListPreference) findPreference("left_swipe_actions");
+        mLeftSwipeActions.setValue(Integer.toString(leftSwipeActions));
+        mLeftSwipeActions.setSummary(mLeftSwipeActions.getEntry());
+        mLeftSwipeActions.setOnPreferenceChangeListener(this);
+
+        rightSwipeActions = Settings.System.getIntForUser(resolver,
+                Settings.System.RIGHT_LONG_BACK_SWIPE_ACTION, 0,
+                UserHandle.USER_CURRENT);
+        mRightSwipeActions = (ListPreference) findPreference("right_swipe_actions");
+        mRightSwipeActions.setValue(Integer.toString(rightSwipeActions));
+        mRightSwipeActions.setSummary(mRightSwipeActions.getEntry());
+        mRightSwipeActions.setOnPreferenceChangeListener(this);
+
+        mLeftSwipeAppSelection = (Preference) findPreference("left_swipe_app_action");
+        boolean isAppSelection = Settings.System.getIntForUser(resolver,
+                Settings.System.LEFT_LONG_BACK_SWIPE_ACTION, 0, UserHandle.USER_CURRENT) == 5/*action_app_action*/;
+        mLeftSwipeAppSelection.setEnabled(isAppSelection);
+
+        mRightSwipeAppSelection = (Preference) findPreference("right_swipe_app_action");
+        isAppSelection = Settings.System.getIntForUser(resolver,
+                Settings.System.RIGHT_LONG_BACK_SWIPE_ACTION, 0, UserHandle.USER_CURRENT) == 5/*action_app_action*/;
+        mRightSwipeAppSelection.setEnabled(isAppSelection);
+        customAppCheck();
+    }
+
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (preference == mLeftSwipeActions) {
+            int leftSwipeActions = Integer.valueOf((String) newValue);
+            Settings.System.putIntForUser(getContentResolver(),
+                    Settings.System.LEFT_LONG_BACK_SWIPE_ACTION, leftSwipeActions,
+                    UserHandle.USER_CURRENT);
+            int index = mLeftSwipeActions.findIndexOfValue((String) newValue);
+            mLeftSwipeActions.setSummary(
+                    mLeftSwipeActions.getEntries()[index]);
+            mLeftSwipeAppSelection.setEnabled(leftSwipeActions == 5);
+            customAppCheck();
+            return true;
+        } else if (preference == mRightSwipeActions) {
+            int rightSwipeActions = Integer.valueOf((String) newValue);
+            Settings.System.putIntForUser(getContentResolver(),
+                    Settings.System.RIGHT_LONG_BACK_SWIPE_ACTION, rightSwipeActions,
+                    UserHandle.USER_CURRENT);
+            int index = mRightSwipeActions.findIndexOfValue((String) newValue);
+            mRightSwipeActions.setSummary(
+                    mRightSwipeActions.getEntries()[index]);
+            mRightSwipeAppSelection.setEnabled(rightSwipeActions == 5);
+            customAppCheck();
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        // Ensure preferences sensible to change get updated
+        actionPreferenceReload();
+        customAppCheck();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        // Ensure preferences sensible to change gets updated
+        actionPreferenceReload();
+        customAppCheck();
+    }
+
+    /* Helper for reloading both short and long gesture as they might change on
+       package uninstallation */
+    private void actionPreferenceReload() {
+        int leftSwipeActions = Settings.System.getIntForUser(getContentResolver(),
+                Settings.System.LEFT_LONG_BACK_SWIPE_ACTION, 0,
+                UserHandle.USER_CURRENT);
+
+        int rightSwipeActions = Settings.System.getIntForUser(getContentResolver(),
+                Settings.System.RIGHT_LONG_BACK_SWIPE_ACTION, 0,
+                UserHandle.USER_CURRENT);
+
+        // Reload the action preferences
+        mLeftSwipeActions.setValue(Integer.toString(leftSwipeActions));
+        mLeftSwipeActions.setSummary(mLeftSwipeActions.getEntry());
+
+        mRightSwipeActions.setValue(Integer.toString(rightSwipeActions));
+        mRightSwipeActions.setSummary(mRightSwipeActions.getEntry());
+
+        mLeftSwipeAppSelection.setEnabled(mLeftSwipeActions.getEntryValues()
+                [leftSwipeActions].equals("5"));
+        mRightSwipeAppSelection.setEnabled(mRightSwipeActions.getEntryValues()
+                [rightSwipeActions].equals("5"));
+    }
+
+    private void customAppCheck() {
+        mLeftSwipeAppSelection.setSummary(Settings.System.getStringForUser(getContentResolver(),
+                String.valueOf(Settings.System.LEFT_LONG_BACK_SWIPE_APP_FR_ACTION), UserHandle.USER_CURRENT));
+        mRightSwipeAppSelection.setSummary(Settings.System.getStringForUser(getContentResolver(),
+                String.valueOf(Settings.System.RIGHT_LONG_BACK_SWIPE_APP_FR_ACTION), UserHandle.USER_CURRENT));
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsProto.MetricsEvent.HORNS;
+    }
+
+    public static final SearchIndexProvider SEARCH_INDEX_DATA_PROVIDER =
+            new BaseSearchIndexProvider() {
+                @Override
+                public List<SearchIndexableResource> getXmlResourcesToIndex(Context context,
+                        boolean enabled) {
+                    final ArrayList<SearchIndexableResource> result = new ArrayList<>();
+                    final SearchIndexableResource sir = new SearchIndexableResource(context);
+                    sir.xmlResId = R.xml.gesture_nav_tweaks;
+                    result.add(sir);
+                    return result;
+                }
+
+                @Override
+                public List<String> getNonIndexableKeys(Context context) {
+                    final List<String> keys = super.getNonIndexableKeys(context);
+                    return keys;
+                }
+            };
+}
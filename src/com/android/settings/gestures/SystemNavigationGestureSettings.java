0274669ce29e8d27f4fcf9a6369c69ba017bf5b9
ezio84 <brabus84@gmail.com>
2020-02-29 15:58:14 +0100

    Allow long back swipe to trigger custom actions/app/activities [2/2]
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- src/com/android/settings/gestures/SystemNavigationGestureSettings.java
+++ src/com/android/settings/gestures/SystemNavigationGestureSettings.java
@@ -130,6 +130,11 @@ public class SystemNavigationGestureSettings extends RadioButtonPickerFragment
     private PreferenceCategory gestureTweaksCategory;
     private SwitchPreference gesturePillToggle;
 
+    private static final String KEY_GESTURE_NAV_TWEAKS_CAT = "gesture_nav_tweaks_category";
+    private static final String KEY_GESTURE_NAV_TWEAKS_PREF = "gesture_nav_custom_options";
+    private PreferenceCategory mGestureTweaksCategory;
+    private Preference mTweaksPreference;
+
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
@@ -158,6 +163,17 @@ public class SystemNavigationGestureSettings extends RadioButtonPickerFragment
                     R.string.navbar_gesture_pill_toggle_title));
         gesturePillToggle.setChecked(getPillToggleState(context) == 1 ? true : false);
         gesturePillToggle.setOnPreferenceChangeListener(this);
+
+        mGestureTweaksCategory = new PreferenceCategory(context);
+        mGestureTweaksCategory.setKey(KEY_GESTURE_NAV_TWEAKS_CAT);
+        mGestureTweaksCategory.setTitle(R.string.navbar_gesture_tweaks_cat_title);
+
+        mTweaksPreference = new Preference(context);
+        mTweaksPreference.setIconSpaceReserved(true);
+        mTweaksPreference.setTitle(R.string.navbar_gesture_tweaks_pref_title);
+        mTweaksPreference.setSummary(R.string.navbar_gesture_tweaks_pref_summary);
+        mTweaksPreference.setKey(KEY_GESTURE_NAV_TWEAKS_PREF);
+        mTweaksPreference.setFragment("com.android.settings.gestures.GestureTweaksSettings");
     }
 
     @Override
@@ -201,6 +217,15 @@ public class SystemNavigationGestureSettings extends RadioButtonPickerFragment
         }
         screen.addPreference(gestureTweaksCategory);
         gestureTweaksCategory.addPreference(gesturePillToggle);
+
+        if (getCurrentSystemNavigationMode(getContext()) == KEY_SYSTEM_NAV_GESTURAL) {
+            screen.addPreference(mGestureTweaksCategory);
+            mGestureTweaksCategory.addPreference(mTweaksPreference);
+            //mTweaksPreference.setEnabled(true);
+        } else {
+            mGestureTweaksCategory.removePreference(mTweaksPreference);
+            screen.removePreference(mGestureTweaksCategory);
+        }
 
         mayCheckOnlyRadioButton();
     }
